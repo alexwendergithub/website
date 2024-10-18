028d45df62cc5d5f6f9fbaae6b9860f299adc832
Alexwender <wender10simoes@gmail.com>
2024-10-18 02:25:13 -0300

    minor changes on dashboard

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,3 +1,5 @@
+import { EditIcon, ViewIcon } from "@chakra-ui/icons"
+
 import { 
   SimpleGrid,
   Text,
@@ -6,31 +8,39 @@ import {
   Image,
   CardHeader,
   CardBody,
-  CardFooter
+  CardFooter,
+  Button,
+  Box
 } from "@chakra-ui/react"
 import { useLoaderData } from "react-router-dom"
 
 export default function Dashboard() {
   const mangas = useLoaderData()
   return (
-    <SimpleGrid spacing={2} minChildWidth={300}>
+    <SimpleGrid spacing={2} minChildWidth={200}>
       {mangas && mangas.map(manga => (
         <Card borderTop="8px" borderColor="blue.400" bg="white">
-          { manga.image != null ? <Image boxSize='150px' objectFit='cover' src={manga.image} alt={manga.title}/>
-            : <Image boxSize='150px' objectFit='cover' src="\images\missing_image.png" alt={manga.title}/>
-          }
           <CardHeader color="gray.700">
             <Text>{manga.title}</Text>
           </CardHeader>
 
           <CardBody color="gray.500">
-            <Link href={manga.link}>
-              <Text width="center">{manga.link}</Text>
-            </Link>
-              <Text width="center" >{manga.chapter}</Text>
+            { manga.image != null ? <Link href={manga.link}><Image width='200px' height='356px' objectFit='cover' src={manga.image} alt={manga.title}/></Link>
+              : <Link href={manga.link}><Image width='200px' height='356px' objectFit='cover' src="\images\missing_image.png" alt={manga.title}/></Link>
+            }
+            <Box>
+              <Text as="span">Chapter:</Text>
+              <Text as="span">{manga.chapter}</Text>
+            </Box>
           </CardBody>
 
           <CardFooter>
+            <Button variant="ghost" leftIcon={<ViewIcon />}>            
+              <Link href={manga.link}>
+                Read
+              </Link>
+            </Button>
+            <Button variant="ghost" leftIcon={<EditIcon />}>Edit</Button>
           </CardFooter>
 
         </Card>
